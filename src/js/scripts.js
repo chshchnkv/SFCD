d3164601e941688416a728dd7945b89cd9b85618
Stan Chashchnikov <chashchnikov@gmail.com>
2015-12-03 17:05:52 +0300

    fixes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,13 @@
 (function () {
   
-  if (window.matchMedia("(max-width: 767px)").matches) {
-    var footerItems = document.querySelectorAll(".page-footer__item");
-    
-    if (footerItems.length > 0) {
-      for (var i=0; i<footerItems.length; i++) {
-        initFooterItem(footerItems[i]);
-      }
+  var footerItems = document.querySelectorAll(".page-footer__item");
+
+  if (footerItems.length > 0) {
+    for (var i=0; i<footerItems.length; i++) {
+      initFooterItem(footerItems[i]);
     }
   }
+  
   
   function initFooterItem(container) {
     var itemHeader = container.querySelector(".page-footer__item-header");
@@ -16,13 +15,15 @@
     if (itemHeader) {
       
       itemHeader.addEventListener("click", function (event) {
-        event.preventDefault();
-        
-        itemHeader.classList.toggle("page-footer__item-header--opened");
+        if (window.matchMedia("(max-width: 767px)").matches) {
+          event.preventDefault();
 
-        var itemContent = itemHeader.parentElement.querySelector(".page-footer__item-content");
-        if (itemContent) {
-          itemContent.classList.toggle("page-footer__item-content--opened");
+          itemHeader.classList.toggle("page-footer__item-header--opened");
+
+          var itemContent = itemHeader.parentElement.querySelector(".page-footer__item-content");
+          if (itemContent) {
+            itemContent.classList.toggle("page-footer__item-content--opened");
+          }
         }
       });
     }    
